89937563e7d76fdff274ff71e5c9c79098e7c87d
Ray Thurman <raymondthurman5@gmail.com>
2020-01-20 09:04:06 -0500

    migration file

--- data/migrations/20200116214644_initial.js
+++ data/migrations/20200116214644_initial.js
@@ -1,8 +1,12 @@
 
-exports.up = function(knex) {
-  
+exports.up = async function (knex) {
+  await knex.schema.createTable('users', users => {
+    users.increments('id');
+    users.string('username').notNullable().unique();
+    users.string('password').notNullable();
+  })
 };
 
-exports.down = function(knex) {
-  
+exports.down = async function (knex) {
+  await knex.schema.dropTableIfExists('users');
 };
